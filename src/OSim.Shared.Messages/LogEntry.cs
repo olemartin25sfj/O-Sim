f0df72774b601d9e26ffbacead91c0c622cd4736
olemartin25sfj <olemartin25fj@gmail.com>
2025-07-29 11:01:05 +0200

    Add Log DTO

---
+++ src/OSim.Shared.Messages/LogEntry.cs
@@ -0,0 +1,8 @@
+namespace OSim.Shared.Messages;
+
+public class LogEntry
+{
+    public DateTime Timestamp { get; set; }
+    public string Service { get; set; } = string.Empty;
+    public string Level { get; set; } = "Info"; // Default to Info, can be changed to Warning, Error, Trace, Debug
+}
