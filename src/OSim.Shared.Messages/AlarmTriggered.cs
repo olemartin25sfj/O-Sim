9a32e524510f14665ef51636039a643193402c5e
olemartin25sfj <olemartin25fj@gmail.com>
2025-07-29 10:58:19 +0200

    add Alarm DTO

---
+++ src/OSim.Shared.Messages/AlarmTriggered.cs
@@ -0,0 +1,9 @@
+namespace OSim.Shared.Messages;
+
+public class AlarmTriggered
+{
+    public DateTime Timestamp { get; set; }
+    public string AlarmType { get; set; } = string.Empty;
+    public string Message { get; set; } = string.Empty;
+    public string Severity { get; set; } = "Warning"; // Default to Warning, can be changed to Critical or Info
+}
