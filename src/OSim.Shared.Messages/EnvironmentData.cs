c5267fdd5822605a384854cb20312a37753bb337
olemartin25sfj <olemartin25fj@gmail.com>
2025-07-29 09:55:22 +0200

    add EnvironmentData DTO

---
+++ src/OSim.Shared.Messages/EnvironmentData.cs
@@ -0,0 +1,16 @@
+namespace OSim.Shared.Messages;
+
+public class EnvironmentData
+{
+    public DateTime Timestamp { get; set; }
+
+    public double WindSpeedKnots { get; set; }
+    public double WindDirection { get; set; }
+
+    public double CurrentSpeed { get; set; }
+    public double CurrentDirection { get; set; }
+
+    public double WaveHeight { get; set; }
+    public double WaveDirection { get; set; }
+    public double WavePeriod { get; set; }
+}
