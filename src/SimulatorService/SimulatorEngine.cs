d9eee378466d06df03c41c88cc11cda591721590
olemartin25sfj <olemartin25fj@gmail.com>
2025-07-30 11:32:48 +0200

    update and expande SimulatorEngine

--- src/SimulatorService/SimulatorEngine.cs
+++ src/SimulatorService/SimulatorEngine.cs
@@ -11,19 +11,56 @@ namespace SimulatorService
         private double _heading = 0.0; // grader
         private double _speed = 10.0; // knop
 
-        public SimulatorEngine()
-        {
+        // Miljøpåvirkning
 
+        private double _windDirection; // grader
+        private double _windSpeed; // knop
+        private double _currentDirection; // strøm
+        private double _currentSpeed; // knop
+
+        public void SetHeading(double heading) => _heading = heading;
+        public void SetSpeed(double speed) => _speed = speed;
+
+        public void SetWind(double directionDeg, double SpeedKnots)
+        {
+            _windDirection = directionDeg;
+            _windSpeed = SpeedKnots;
+        }
+
+
+        public void SetCurrent(double directionDeg, double SpeedKnots)
+        {
+            _currentDirection = directionDeg;
+            _currentSpeed = SpeedKnots;
         }
 
         public void Update(TimeSpan deltaTime)
         {
             double seconds = deltaTime.TotalSeconds;
-            double distanceNm = _speed * (seconds / 3600.0); // nm = knop * timer
-            double distanceDeg = distanceNm / 60.0; // ca 1° = 60nm
+            double distanceShip = _speed * (seconds / 3600.0);
+            double distanceWind = _windSpeed * (seconds / 3600.0);
+            double distanceCurrent = _currentSpeed * (seconds / 3600.0);
 
-            _latitude += distanceDeg * Math.Cos(_heading * Math.PI / 180.0);
-            _longitude += distanceDeg * Math.Sin(_heading * Math.PI / 180.0);
+            // Totale bevegelser i X/Y-retning i grader
+
+            double dx = 0.0;
+            double dy = 0.0;
+
+            // Fartøyets bevegelse
+            dx += distanceShip * Math.Cos(DegToRad(_heading));
+            dy += distanceShip * Math.Sin(DegToRad(_heading));
+
+            // Vinddrift
+            dx += distanceWind * Math.Cos(DegToRad(_windDirection));
+            dy += distanceWind * Math.Sin(DegToRad(_windDirection));
+
+            // Strømdrift
+            dx += distanceCurrent * Math.Cos(DegToRad(_currentDirection));
+            dy += distanceCurrent * Math.Sin(DegToRad(_currentDirection));
+
+            // Endre posisjon (1° = ca 60 nm)
+            _latitude += dy / 60.0;
+            _longitude += dx / 60.0;
         }
 
         public VesselState GetCurrentState()
@@ -38,7 +75,6 @@ namespace SimulatorService
             };
         }
 
-        public void SetHeading(double heading) => _heading = heading;
-        public void SetSpeed(double speed) => _speed = speed;
+        private static double DegToRad(double deg) => deg * Math.PI / 180.0;
     }
 }
